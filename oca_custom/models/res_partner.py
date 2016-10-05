ba53a515bd09b4807a007fc4e017824c9fcc2d68
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-10-05 21:59:01 +0200

    [IMP] avoid to crash during analysis, if repository is not correct; [IMP] remove warning when analysing Odoo modules manifest, if docutils raise warning

---
+++ oca_custom/models/res_partner.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today: Odoo Community Association (OCA)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    github_login = fields.Char(readonly=False)
+
+    x_github_login = fields.Char(related='github_login', store=True)
