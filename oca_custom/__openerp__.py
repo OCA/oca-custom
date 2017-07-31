5931eb292873a8ed87059caa4f115cdaf7e87411
Holger Brunn <hbrunn@therp.nl>
2017-07-31 17:33:12 +0200

    [ADD] initial 10.0 commit

--- oca_custom/__openerp__.py
+++ oca_custom/__openerp__.py
@@ -5,7 +5,7 @@
 {
     'name': 'OCA Custom Settings',
     'summary': 'Custom Settings for OCA Instance',
-    'version': '8.0.0.0.0',
+    'version': '10.0.0.0.0',
     'category': 'Custom',
     'author': [
         'GRAP',
