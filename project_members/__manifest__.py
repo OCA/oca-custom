514bb1842fe968a83a37626e88729399186761eb
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2018-01-09 15:57:30 +0100

    [ADD] project_members module

---
+++ project_members/__manifest__.py
@@ -0,0 +1,7 @@
+{'name': 'project members',
+ 'description': 'add members to projects',
+ 'depends': ['project'],
+ 'data': ['views/project.xml'],
+ 'license': 'AGPL-3',
+ 'authors': 'Alexandre Fayolle <alexandre.fayolle@camptocamp.com>, Odoo Community Association (OCA)',
+}
