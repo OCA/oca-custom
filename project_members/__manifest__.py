2426e0f87a7e0140bd99489d71a10f8f149efafc
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2018-06-25 12:31:39 +0200

    fix flake8

--- project_members/__manifest__.py
+++ project_members/__manifest__.py
@@ -1,7 +1,9 @@
-{'name': 'project members',
- 'description': 'add members to projects',
- 'depends': ['project'],
- 'data': ['views/project.xml'],
- 'license': 'AGPL-3',
- 'authors': 'Alexandre Fayolle <alexandre.fayolle@camptocamp.com>, Odoo Community Association (OCA)',
+{
+    'name': 'project members',
+    'de scription': 'add members to projects',
+    'depends': ['project'],
+    'data': ['views/project.xml'],
+    'license': 'AGPL-3',
+    'authors': ('Alexandre Fayolle <alexandre.fayolle@camptocamp.com>,'
+                'Odoo Community Association (OCA)'),
 }
--- project_members/models/project.py
+++ project_members/models/project.py
@@ -4,5 +4,10 @@ from odoo import models, fields
 class ProjectProject(models.Model):
     _inherit = 'project.project'
 
-
-    members = fields.Many2many('res.users', 'project_user_rel', 'project_id', 'uid', 'Project Members')
+    members = fields.Many2many(
+        'res.users',
+        'project_user_rel',
+        'project_id',
+        'uid',
+        'Project Members'
+    )
