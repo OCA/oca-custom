fdc3d3f683fddf4598076362495984dbb06408e6
st-aprajapati <aprajapati@surekhatech.com>
2018-08-24 18:23:08 +0530

    [IMP] for portal test and while saving portal page details.

--- website_oca_integrator/tests/test_ui_portal.py
+++ website_oca_integrator/tests/test_ui_portal.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 import odoo.tests
+from odoo.api import Environment
 
 
 @odoo.tests.common.at_install(False)
@@ -15,3 +16,14 @@ class TestUi(odoo.tests.HttpCase):
                         "odoo.__DEBUG__.services['web_tour.tour']"
                         ".tours.integrator_portal.ready",
                         login="integrator")
+
+        cr = self.registry.cursor()
+        assert cr == self.registry.test_cr
+        env = Environment(cr, 1, {})
+        partner = env['res.partner'].sudo().search(
+            [('github_organization', '=', 'test_github_organization')
+             ])
+
+        self.assertEqual(len(partner), 1)
+        self.assertTrue(set(partner.favourite_module_ids.ids).issubset(
+            set(partner.developed_module_ids.ids)), True)
