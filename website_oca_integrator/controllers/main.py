e5e706c58523f29b467bf44253fa3f615bc6d481
Graeme Gellatly <gdgellatly@gmail.com>
2019-03-02 11:06:46 +1300

    Improve Ordering of Search Results in Integrator Pages

--- website_oca_integrator/controllers/main.py
+++ website_oca_integrator/controllers/main.py
@@ -81,7 +81,10 @@ class WebsiteIntegrator(http.Controller):
 
         # search integrators matching current search parameters
         integrator_ids = partner_obj.sudo().search(
-            base_integrator_domain, order="grade_id DESC, display_name ASC",
+            base_integrator_domain, 
+            order="grade_id ASC NULLS LAST, implemented_count DESC,"
+                  "contributor_count DESC, member_count DESC,"
+                  "display_name ASC",
             offset=pager['offset'], limit=self._references_per_page)
         integrators = integrator_ids.sudo()
 
