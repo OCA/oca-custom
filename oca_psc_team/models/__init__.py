003b7b26eb490fee847691315a845f35c5d7b722
st-aprajapati <aprajapati@surekhatech.com>
2019-04-10 14:48:37 +0530

    [ADD] 'oca_psc_team' and 'website_oca_psc_team' modules.

---
+++ oca_psc_team/models/__init__.py
@@ -0,0 +1,3 @@
+from . import project
+from . import github_repository
+from . import psc_category
---
+++ oca_psc_team/models/psc_category.py
@@ -0,0 +1,26 @@
+# Copyright 2019 Surekha Technologies (https://www.surekhatech.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class PscCategory(models.Model):
+    _name = 'psc.category'
+    _order = 'sequence'
+    _description = 'PSC Categories'
+
+    name = fields.Char(
+        required=True,
+    )
+
+    description = fields.Text(
+        required=True,
+    )
+
+    sequence = fields.Integer()
+
+    project_ids = fields.One2many(
+        comodel_name='project.project',
+        inverse_name='psc_category_id',
+        string='Project'
+    )
