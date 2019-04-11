3af4868e63087df0456cda7a39946020def304f0
st-aprajapati <aprajapati@surekhatech.com>
2019-04-11 14:27:36 +0530

    [12.0][MIG] 'oca_psc_team' and 'website_oca_psc_team': Migration to v12

--- oca_psc_team/__manifest__.py
+++ oca_psc_team/__manifest__.py
@@ -4,7 +4,7 @@
     'name': 'OCA PSC Team',
     'summary': 'This module allows you to store PSC details in project.'
                'These details will be displayed in PSC Teams in website.',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Website',
     'license': 'AGPL-3',
     'website': 'https://github.com/OCA/oca-custom',
