c91d939ef6ec4330934aca46564cbdc9e50d5fe7
Jairo Llopis <yajo.sk8@gmail.com>
2019-04-24 13:53:10 +0100

    [FIX] website_oca_integrator: Fix tour
    Remove unneeded, failing, step, move tours to demo data and lint the file.

--- website_oca_integrator/tests/test_ui_portal.py
+++ website_oca_integrator/tests/test_ui_portal.py
@@ -2,7 +2,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 import odoo.tests
-from odoo.api import Environment
 
 
 @odoo.tests.common.at_install(False)
@@ -10,16 +9,14 @@ from odoo.api import Environment
 class TestUi(odoo.tests.HttpCase):
 
     def test_integrator_portal(self):
-        self.phantom_js("/",
+        self.browser_js("/my/account",
                         "odoo.__DEBUG__.services['web_tour.tour']"
                         ".run('integrator_portal')",
                         "odoo.__DEBUG__.services['web_tour.tour']"
                         ".tours.integrator_portal.ready",
                         login="integrator")
 
-        cr = self.registry.cursor()
-        assert cr == self.registry.test_cr
-        env = Environment(cr, 1, {})
+        env = self.env
         partner = env['res.partner'].sudo().search(
             [('github_organization', '=', 'test_github_organization')
              ])
