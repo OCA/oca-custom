a7d994b9c494bd6e337600a9d6d57a7613e7de9d
Arnaud LAYEC <arnaudlayec@gmail.com>
2026-06-26 13:17:15 +0200

    [IMP] oca_membership: add field 'is_membership_invoice' for communication templates

--- oca_membership/__manifest__.py
+++ oca_membership/__manifest__.py
@@ -5,12 +5,13 @@
 
 {
     "name": "OCA Membership (custom)",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.2",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/oca-custom",
     "license": "AGPL-3",
     "category": "Custom",
     "depends": [
+        "account",
         "membership_extension",  # for membership.category
         "oca_vcp",
     ],
