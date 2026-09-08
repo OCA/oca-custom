d8551c0995904da293163793685b4c8aeb21d749
Arnaud LAYEC <arnaudlayec@gmail.com>
2026-09-08 17:31:21 +0200

    [IMP] oca_search_engine: add integrators' url_key

--- oca_search_engine/schemas/res_partner_company.py
+++ oca_search_engine/schemas/res_partner_company.py
@@ -168,7 +168,9 @@ class Company(StrictExtendableBaseModel):
             members_count=len(record.organization_member_ids.filtered("is_member")),
             modules_count=record.modules_author_count,
             # technical website fields
-            url_key=record.is_sponsor and record.url_key or None,
+            url_key=(
+                (record.is_sponsor or record.is_integrator) and record.url_key or None
+            ),
             redirect_url_key=record.redirect_url_key or [],
             # sponsorship
             sponsorship=None
